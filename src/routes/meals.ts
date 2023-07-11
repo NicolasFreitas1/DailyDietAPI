8c07bd26c0788981c7a0212cb1d0135fb9927ac7
Nicolas Freitas <nicolas.freitas@agpr5.com>
2023-07-11 11:43:45 -0300

    :sparkles: feat: adicionado criação de refeição

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -2,6 +2,36 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { knex } from "../database";
 import { randomUUID } from "node:crypto";
+import { checkUserIdExists } from "../middlewares/check-user-id-exists";
 // import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
 
-export async function mealsRoutes(app: FastifyInstance) {}
+export async function mealsRoutes(app: FastifyInstance) {
+  app.post(
+    "/",
+    {
+      preHandler: [checkUserIdExists],
+    },
+    async (request, reply) => {
+      const createMealsSchema = z.object({
+        meal_name: z.string(),
+        meal_description: z.string(),
+        is_inside: z.boolean(),
+      });
+
+      const { meal_name, meal_description, is_inside } =
+        createMealsSchema.parse(request.body);
+
+      const { user_id } = request.cookies;
+
+      await knex("meals").insert({
+        meal_id: randomUUID(),
+        meal_name,
+        meal_description,
+        is_inside,
+        user_id,
+      });
+
+      return reply.status(201).send();
+    }
+  );
+}
