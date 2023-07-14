d56b275efba4e5a1d1346b1f4154268b19cf0501
Nicolas Freitas <nicolas.freitas@agpr5.com>
2023-07-14 08:41:35 -0300

    :bug: fix: corrigido tipagem

--- src/utils/calcBestSequenceDiet.ts
+++ src/utils/calcBestSequenceDiet.ts
@@ -1,31 +1,30 @@
 interface IMealParam {
-    id: string
-    userId: string
-    name: string
-    description?: string | undefined
-    mealHour: string
-    isInDiet: boolean
-   
-  }
-  
-  export function calcBestSequenceDiet(meals: IMealParam[]): number {
-    const listSequence: number[] = []
-    let countSequence: number = 0
-  
-    const listIsInDiet = meals.map(({ isInDiet }) => isInDiet)
-  
-    listIsInDiet.forEach((item, index) => {
-      if (item) {
-        countSequence++
-      } else {
-        listSequence.push(countSequence)
-        countSequence = 0
-      }
-  
-      if (listIsInDiet.length - 1 === index) {
-        listSequence.push(countSequence)
-      }
-    })
-  
-    return Math.max(...listSequence)
-  }
+  meal_id: string;
+  meal_name: string;
+  meal_description: string;
+  meal_date_time: string;
+  is_inside: boolean;
+  user_id: string;
+}
+
+export function calcBestSequenceDiet(meals: IMealParam[]): number {
+  const listSequence: number[] = [];
+  let countSequence: number = 0;
+
+  const listIsInDiet = meals.map(({ is_inside }) => is_inside);
+
+  listIsInDiet.forEach((item, index) => {
+    if (item) {
+      countSequence++;
+    } else {
+      listSequence.push(countSequence);
+      countSequence = 0;
+    }
+
+    if (listIsInDiet.length - 1 === index) {
+      listSequence.push(countSequence);
+    }
+  });
+
+  return Math.max(...listSequence);
+}
